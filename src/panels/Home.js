36fd5c0401b05e4aa4e2b473305b28b630f40d95
trueprogr <ilovememesdintkillme@gmail.com>
2020-11-01 00:54:36 +0900

    button+hide

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -5,42 +5,58 @@ import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import $ from 'jquery';
+import { render } from 'react-dom';
+import { Icon16Add} from '@vkontakte/icons';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Component from 'react';
 
+class Home extends Component{
+	constructor(){
+		super()
+		this.state={
+			showMe:false
+		}
+	}
+}
+operation()
+{
+	this.setState({
+		showMe:true
+	})
+}
+function Home({ id, go, fetchedUser }) {
+	return (
+		<Panel id={id}>
+			<PanelHeader>Налоговый калькулятор</PanelHeader>
 
-$(".plus").click(function() {
-	$(".add").toggleClass("rotate");
-	$(".plus").toggleClass("open");
-	$(".pop").slideToggle(400);
-  });
-  
+			<Group title="Посчитать НДС" plus>
+				<Cell onClick={go} data-to="nds" >
+				<Div>{
+					this.state.showMe?
+					<div>sample text</div>
+					:null
+					}
+       <Button before={<Icon16Add/>} onClick={()=>this.operation()}></Button>
+     </Div>
+					НДС
+				</Cell>
+			</Group>
+			<Group title="Посчитать НДФЛ">
+				<Cell onClick={go} data-to="NDFL">
+					НДФЛ
+				</Cell>
+			</Group>
+			<Group title="Посчитать транспортный налог">
+				<Cell onClick={go} data-to="Trans">
+					Транспортный налог
+				</Cell>
+			</Group>
 
-const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		<PanelHeader>Налоговый калькулятор</PanelHeader>
-
-		<Group title="Посчитать НДС">
-			<Cell onClick={go} data-to="nds">
-			НДС	
-			</Cell>
-		</Group>
-		<Group title="Посчитать НДФЛ">
-			<Cell onClick={go} data-to="NDFL">
-			НДФЛ	
-			</Cell>
-		</Group>
-		<Group title="Посчитать транспортный налог">
-			<Cell onClick={go} data-to="Trans">
-			Транспортный налог	
-			</Cell>
-		</Group>
-		{/* <Group title="Посчитать имущественный налог">
-			<Cell onClick={go} data-to="Immus">
-			Имущественный налог	
-			</Cell>
-		</Group> */}
-	</Panel>
-);
-
+		</Panel>
+	);
+}
+		
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
