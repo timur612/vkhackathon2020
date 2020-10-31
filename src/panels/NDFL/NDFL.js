4aa8e959a2711cc50dcc9dc248cba246ab15fbfe
timur612 <tz220404@gmail.com>
2020-10-31 13:29:38 +0900

    ndfl

--- src/panels/NDFL/NDFL.js
+++ src/panels/NDFL/NDFL.js
@@ -9,7 +9,8 @@ import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 
-import {FormLayout,FormLayoutGroup,Input} from '@vkontakte/vkui';
+import {FormLayout,FormLayoutGroup,Input,SelectMimicry,View,Root,Group,List,Cell} from '@vkontakte/vkui';
+import Icon24Done from '@vkontakte/icons/dist/24/done'
 
 const styles = {
     btn: {
@@ -20,33 +21,75 @@ const styles = {
 const osName = platform();
 
 const NDFL = props =>{
+    const [country,setCountry] = React.useState({country:''});
+    const [activeView,setActiveView] = React.useState({activeView: 'profile'});
     return (
-        <Panel id={props.id}>
-            <PanelHeader
-                left={<PanelHeaderButton onClick={props.go} data-to="home">
-                    {osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-                </PanelHeaderButton>}
-            >
-                Расчет НДФЛ 
-            </PanelHeader>
-            
-                <FormLayout>
-                    <FormLayoutGroup top="НДС из">
-                        <Input type="text" />
-                    </FormLayoutGroup>
+        <Root activeView={activeView}>
+            <View activePanel="profile" id="profile">
+                <Panel id="profile">
+                    <PanelHeader
+                        left={<PanelHeaderButton onClick={props.go} data-to="home">
+                            {osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
+                        </PanelHeaderButton>}
+                    >
+                        Расчет НДФЛ 
+                    </PanelHeader>
 
-                    <FormLayoutGroup top="Ставка(%)">
-                        <Input type="text" />
-                    </FormLayoutGroup>
-                </FormLayout>
-            
-            <Div style={styles.btn}>
-                <Button size="xl" level="2">
-                    Рассчитать налог
-                </Button>
-            </Div>
-            
-	    </Panel>
+                        <FormLayout>
+                            <SelectMimicry
+                            top="Выберите страну"
+                            placeholder="Не выбрана"
+                            onClick={() => setActiveView({ activeView: 'countries' })}
+                            >{country}</SelectMimicry>
+
+                            <FormLayoutGroup top="Сумма">
+                                <Input type="text" />
+                            </FormLayoutGroup>
+
+                            <FormLayoutGroup top="Ставка(%)">
+                                <Input type="text" />
+                            </FormLayoutGroup>
+                        </FormLayout>
+                    
+                    <Div style={styles.btn}>
+                        <Button size="xl" level="2">
+                            Рассчитать налог
+                        </Button>
+                    </Div>
+                    
+                </Panel>
+            </View>
+
+            <View activePanel="countries" id="countries">
+                <Panel id="countries">
+                    <PanelHeader>
+                        Выбор страны
+                    </PanelHeader>
+                    <Group>
+                        <List>
+                        <Cell
+                            onClick={() => {setCountry({country: 'Россия'}); setActiveView({activeView: 'profile' })}}
+                            asideContent={country === 'Россия' ? <Icon24Done fill="var(--accent)" /> : null}
+                        >
+                            Россия
+                        </Cell>
+                        <Cell
+                            onClick={() => {setCountry({country: 'Италия'}); setActiveView({activeView: 'profile' })}}
+                            asideContent={country === 'Италия' ? <Icon24Done fill="var(--accent)" /> : null}
+                        >
+                            Италия
+                        </Cell>
+                        <Cell
+                            onClick={() => {setCountry({country: 'Англия'}); setActiveView({activeView: 'profile' })}}
+                            asideContent={country === 'Англия' ? <Icon24Done fill="var(--accent)" /> : null}
+                        >
+                            Англия
+                        </Cell>
+                        </List>
+                    </Group>
+                </Panel>
+            </View>
+        </Root>
     );
 }
 
