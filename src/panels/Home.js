bd1814f0cd78c60a60c34ac3b93c5e7f69481b46
trueprogr <ilovememesdintkillme@gmail.com>
2020-10-31 11:17:01 +0900

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -10,7 +10,7 @@ import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
 const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Кто зашел тот лох</PanelHeader>
+		<PanelHeader>Кто зашел тот лох трусы в горох</PanelHeader>
 		{fetchedUser &&
 		<Group title="User Data Fetched with VK Bridge">
 			<Cell
